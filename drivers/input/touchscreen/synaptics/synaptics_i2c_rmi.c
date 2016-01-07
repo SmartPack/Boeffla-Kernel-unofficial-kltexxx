5c9d21c4a5c2765529df71f154fbb945975f37d9
andip71 <andreasp@gmx.de>
2016-01-07 12:33:51 +0100

    kt_wake_funcs: Fix proximity sensor handling for screen wake and sleep functions (CM13 version)
    proximity sensor was not being used at all before

--- drivers/input/touchscreen/synaptics/synaptics_i2c_rmi.c
+++ drivers/input/touchscreen/synaptics/synaptics_i2c_rmi.c
@@ -5731,6 +5731,7 @@ static int synaptics_rmi4_stop_device(struct synaptics_rmi4_data *rmi4_data)
 	if (wake_options_okto_enable())
 	{
 		enable_irq_wake(rmi4_data->i2c_client->irq);
+		wake_funcs_set_prox(true);
 		screen_wake_options_when_off = screen_wake_options;
 	}
 	else
@@ -5794,6 +5795,7 @@ static int synaptics_rmi4_start_device(struct synaptics_rmi4_data *rmi4_data)
 	if (screen_wake_options_when_off)
 	{
 		disable_irq_wake(rmi4_data->i2c_client->irq);
+		wake_funcs_set_prox(false);
 	}
 	else
 		enable_irq(rmi4_data->i2c_client->irq);
