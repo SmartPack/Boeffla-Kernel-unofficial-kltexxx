3fd4f1b46a3e5b9cb8e357b159a886170cabfeda
andip71 <andreasp@gmx.de>
2016-12-22 09:18:26 +0100

    Boeffla ramfs: Fix UMS mode for CM14

--- anykernel_boeffla/ramdisk/res/bc/bccontroller.sh
+++ anykernel_boeffla/ramdisk/res/bc/bccontroller.sh
@@ -1250,14 +1250,22 @@ fi
 
 if [ "apply_ums" == "$1" ]; then
 	if [ "1" == "$2" ]; then
+		echo "1" > /sys/devices/msm_dwc3/f9200000.dwc3/gadget/lun0/nofua
+		echo "0" > /sys/devices/msm_dwc3/f9200000.dwc3/gadget/lun0/ro
 		echo "0" > /sys/devices/msm_dwc3/f9200000.dwc3/gadget/lun0/cdrom
-		/system/bin/setprop persist.sys.usb.config mass_storage,adb
 		echo "/dev/block/mmcblk1p1" > /sys/devices/msm_dwc3/f9200000.dwc3/gadget/lun0/file
+		echo 0 > /sys/class/android_usb/android0/enable;
+		echo mass_storage > /sys/class/android_usb/android0/functions
+		echo 1 > /sys/class/android_usb/android0/enable
+		setprop sys.usb.config mass_storage,adb
 	fi
 
 	if [ "0" == "$2" ]; then
 		echo "" > /sys/devices/msm_dwc3/f9200000.dwc3/gadget/lun0/file
-		/system/bin/setprop persist.sys.usb.config mtp,adb
+		echo 0 > /sys/class/android_usb/android0/enable;
+		echo mtp > /sys/class/android_usb/android0/functions
+		echo 1 > /sys/class/android_usb/android0/enable
+		setprop sys.usb.config mtp,adb
 	fi
 	exit 0
 fi
