da21c13bc18c50fc87149aaadb975b0d84b6aad5
andip71 <andreasp@gmx.de>
2018-03-22 13:05:11 +0100

    Boeffla ramfs: Remove ext4 tweaks and dynamic fsync enabled by default

--- anykernel_boeffla/ramdisk/res/bc/boeffla-init-bc.sh
+++ anykernel_boeffla/ramdisk/res/bc/boeffla-init-bc.sh
@@ -79,12 +79,7 @@
 
 # Apply Boeffla-Kernel default settings 1
 
-	# Ext4 tweaks default to on
-	/sbin/busybox sync
-	mount -o remount,commit=20,noatime $CACHE_DEVICE /cache
-	/sbin/busybox sync
-	mount -o remount,commit=20,noatime $DATA_DEVICE /data
-	/sbin/busybox sync
+	# currently no tweaks
 
 	echo $(date) Boeffla-Kernel default settings 1 applied >> $BOEFFLA_LOGFILE
 
@@ -144,11 +139,7 @@
 		. $BOEFFLA_STARTCONFIG
 		echo $(date) Startup configuration applied  >> $BOEFFLA_LOGFILE
 	else
-		# dynamic fsync to on
-		echo 1 > /sys/kernel/dyn_fsync/Dyn_fsync_active
-		/sbin/busybox sync
-
-		echo $(date) "No startup configuration found, enable all default settings"  >> $BOEFFLA_LOGFILE
+		echo $(date) "No startup configuration found"  >> $BOEFFLA_LOGFILE
 	fi
 
 # Turn off debugging for certain modules
