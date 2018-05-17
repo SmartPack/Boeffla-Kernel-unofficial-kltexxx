d31974fadf7993760098f84ae81962908c4324d3
engstk <eng.stk@sapo.pt>
2018-05-17 18:07:12 +0900

    cpufreq: Adding cpufreq_blu_active
    Signed-off-by: engstk <eng.stk@sapo.pt>
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -497,6 +497,9 @@ extern struct cpufreq_governor cpufreq_gov_ondemandplus;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKACTIVE)
 extern struct cpufreq_governor cpufreq_gov_yankactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_yankactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_ACTIVE)
+extern struct cpufreq_governor cpufreq_gov_blu_active;
+#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_blu_active)
 #endif
 
 
